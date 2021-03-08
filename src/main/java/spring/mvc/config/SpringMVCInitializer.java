d106874149f3b122692fcee9af32cacf1e2ef8bf
Pempium4 <front.shipper@gmail.com>
2021-03-08 19:27:39 +0200

    Java config

---
+++ src/main/java/spring/mvc/config/SpringMVCInitializer.java
@@ -0,0 +1,20 @@
+package spring.mvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMVCInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
