e50512c1e70240e60815318b0dda3f008436b982
Pempium4 <front.shipper@gmail.com>
2021-03-12 11:36:21 +0200

    Simple Server Calculator

--- src/main/java/spring/mvc/controller/FirstController.java
+++ src/main/java/spring/mvc/controller/FirstController.java
@@ -1,6 +1,7 @@
 package spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +14,12 @@ public class FirstController {
 
     @GetMapping("/hello")
     //получаем доступ ко всему объекту request :/
-    public String helloPage(HttpServletRequest request){
+    public String helloPage(HttpServletRequest request, Model model){
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
 
-        System.out.println("Hello, " + firstName + " " + lastName);
-
+//        System.out.println("Hello, " + firstName + " " + lastName);
+        model.addAttribute("message", "Hello, " + firstName + " " + lastName);
         return "first/hello_page";
     }
 
@@ -26,9 +27,34 @@ public class FirstController {
     //создаем лишь нужные getParams :)
     //404 если не передать параметры
     public String goodbyePage(@RequestParam("firstName") String firstName,
-                              @RequestParam(value="lastName", required = false) String lastName){
-        System.out.println("Goodbye, " + firstName + " " + lastName);
+                              @RequestParam(value="lastName", required = false) String lastName,
+                              Model model){
+//        System.out.println("Goodbye, " + firstName + " " + lastName);
+        model.addAttribute("message", "Goodbye, " + firstName + " " + lastName);
 
         return "first/goodbye_page";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam("a") int var1,
+                                 @RequestParam("b") int var2,
+                                 @RequestParam("action") String action,
+                                 Model model){
+        double result = 0;
+        switch (action){
+            case "multiplication":
+                result = var1 * var2;
+                break;
+            case "addition":
+                result = var1 + var2;
+                break;
+            case "subtraction":
+                result = var1 - var2;
+                break;
+            case "division":
+                result = var1 / (double)var2;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator_page";
     }
 }
