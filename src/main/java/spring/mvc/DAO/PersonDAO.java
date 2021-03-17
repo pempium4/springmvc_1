dc1839b95430a4d629d01b65621867e10a88c805
Pempium4 <front.shipper@gmail.com>
2021-03-17 18:50:35 +0200

    Inject PreparedStatement

--- src/main/java/spring/mvc/DAO/PersonDAO.java
+++ src/main/java/spring/mvc/DAO/PersonDAO.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int ID;
 
 //    @Value("${database.URL}")
     private static String URL = "jdbc:mysql://localhost:3306/users_db";
@@ -35,13 +34,22 @@ public class PersonDAO {
     }
 
     public Person index(int id){
-//        for (Person person:personList) {
-//            if(person.getId() == id){
-//                return person;
-//            }
-//        }
-        return null;
-        //return personList.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        Person person = null;
+        try {
+            PreparedStatement preparedStatement
+                    = connection.prepareStatement("SELECT * FROM person WHERE id=?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setAge(resultSet.getInt("age"));
+            person.setEmail(resultSet.getString("email"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return person;
     }
 
     public List<Person> show(){
@@ -68,25 +76,38 @@ public class PersonDAO {
 
     public void save(Person person){
         try {
-            Statement statement = connection.createStatement();
-            String SQL = "INSERT INTO Person VALUES(" + ++ID + ",'"
-                    + person.getName() +
-                    "'," + person.getAge() + ",'"
-                    + person.getEmail() + "')";
-            statement.executeUpdate(SQL);
+            PreparedStatement preparedStatement
+                    = connection.prepareStatement("INSERT INTO person VALUES(1, ?, ?, ?)");
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
+            preparedStatement.executeUpdate();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
     public void update(int id, Person person){
-//        Person personToBeUpdated = index(id);
-//        personToBeUpdated.setName(person.getName());
-//        personToBeUpdated.setAge(person.getAge());
-//        personToBeUpdated.setEmail(person.getEmail());
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE person SET name=?, age=?, email=? WHERE id=?");
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
+            preparedStatement.setInt(4, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
     public void delete(int id){
-//        Person person = index(id);
-//        personList.remove(person);
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("DELETE FROM person WHERE id=?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
